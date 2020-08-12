bc0417313a90452c6946792aff8d0e4e187ca3d6
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-12 17:24:12 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#9d9d9d" });
+ });
+
+$(function () {
+      $(".rslides").responsiveSlides({
+		manualControls: '#slider-pager',  
+        speed: 800,
+		timeout: 8000,
+		nav:true,
+		
+		
+});
+});
+
+	/*Fancybox*/
+
+
+            var forms = $('.form-modal'),
+            cb_input = forms.find('input[type=text], input[type=email], input[type=tel] , textarea'),
+            cr_close = $.fancybox.close();
+
+    $("#fancybox-overlay").fancybox({
+        padding: [0, 0, 0, 0],
+        margin: [120, 0, 0, 0],
+        scrolling: 'visible',
+        minWidth: 325,
+        minHeight: 330,
+        autoSize: false,
+        autoHeight: true,
+        autoWidth: true,
+        maxWidth: 9999,
+        maxHeight: 9999,
+        fixed: false,
+        autoCenter: false,
+        closeBtn: true,
+        afterClose: function () {
+            cb_input.val('');
+        }
+    });
+
+    $('.btr').click(function () {
+        $.fancybox.close();
+    });
+	
+
+/*Fancybox end*/  
+
+	
+		
+		//   $("a[href$='.jpg'], a[href$='.png']")
+//           .addClass("fancybox");
+          
+           $("a[href$='.jpg'], a[href$='.png']").each(function() {
+      $(this).addClass("fancybox");
+       $(this).attr('rel', 'gallery');
+     
+});
+
+ $(".fancybox").fancybox({
+        padding: [0, 0, 0, 0],
+        openEffect: 'elastic',
+        openSpeed: 300,
+//        closeEffect: 'elastic',
+//        scrolling: 'visible',
+         background:'white',
+        maxWidth: 800,
+		arrows: false,
+        fixed: false,
+        autoCenter: false,
+     
+    });
+	
+	 $(document).ready(function(){
+   $(' a[href*=#]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 1400);
+      e.preventDefault();
+   });
+   return false;
+});
